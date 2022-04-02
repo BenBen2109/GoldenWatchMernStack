c48e10fea2d5d4b125c8653ad8b78f9e384b1f04
Ben <leduykhanh.ldk21092000@gmail.com>
2022-04-02 16:12:46 +0700

    lam toi client dang loi render

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(fileUpload({
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
 app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
 
 // app.get('/', (req, res) => {
 //     res.json({msg: "KN thành công"})
